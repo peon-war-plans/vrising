5fdc93b915faee420a41ab38410675056fb88559
Umlatt <theplacetosendmemail@gmail.com>
2022-06-04 10:48:59 +0000

    Added configfile check

--- server_start.sh
+++ server_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 logfile="/var/log/peon/${0##*/}.log"
 rootpath="/home/steam/steamcmd/"
+configpath="/home/steam/steamcmd/peon/config"
 cd $rootpath
 echo "" > $logfile
 # Logging config start - capture all
@@ -9,6 +10,17 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$logfile 2>&1
 # Logging config end
 echo "##################### STARTING SERVER ######################"
+# FIRST RUN CHECK
+echo ""
+if [ ! -f "$configpath/ServerGameSettings.json" ]; then
+        echo "$configpath/ServerGameSettings.json not found. Copying default file."
+        cp "$rootpath/data/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json" "$configpath" 2>&1
+fi
+if [ ! -f "$configpath/ServerHostSettings.json" ]; then
+        echo "$configpath/ServerHostSettings.json not found. Copying default file."
+        cp "$rootpath/data/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json" "$configpath" 2>&1
+fi
+
 # CUSTOM GAME SERVER COMMAND
 echo "Clean existing /tmp/.X0-lock"
 rm -rf /tmp/.X0-lock 2>&1
@@ -16,5 +28,5 @@ echo " "
 echo "Starting Xvfb"
 Xvfb :0 -screen 0 1024x768x16 &
 echo "Start server"
-#DISPLAY=:0.0 wine64 data/VRisingServer.exe -persistentDataPath /home/steam/steamcmd/peon/config -serverName "$SERVERNAME" -saveName "$WORLDNAME" -logFile "/var/log/peon/server.log" "$GAMEPORT" "$QUERYPORT" 2>&1
+#DISPLAY=:0.0 wine64 data/VRisingServer.exe -persistentDataPath $configpath -serverName "$SERVERNAME" -saveName "$WORLDNAME" -logFile "/var/log/peon/server.log" "$GAMEPORT" "$QUERYPORT" 2>&1
 bash
